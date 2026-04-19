cb534d344c6b28d02604eb762d1e877e6197272a
laurikomulainen <lakom94@gmail.com>
2026-04-19 11:55:59 +0300

    Add separate model config for MASS dataset

---
+++ configs/mass_model_config.py
@@ -0,0 +1,30 @@
+TRAINING_PARAMS = {
+    'batch_size': 16,
+    'learning_rate': 1e-4,
+    'dropout_rate': 0.2,
+    'weight_decay': 1e-4,
+    'num_epochs': 40,
+    'early_stopping_patience': 8,
+    'use_swa': True,
+    'use_gating_branch': True,
+    'seg_loss_weight': 0.85,
+    'kernel_size': 11,
+}
+TRAINING_PARAMS['padding'] = (TRAINING_PARAMS['kernel_size'] - 1) // 2
+
+INFERENCE_PARAMS = {
+    'iou_threshold': 0.2,
+    'fixed_threshold': 0.5,
+    'inference_mode': 'ensemble', # Options: none (best), swa, ensemble
+}
+
+POST_PROCESSING_PARAMS = {
+    'min_duration_sec': 0.5,
+    'gap_thresh_sec': 0.3,
+    'fixed_border_thresh': 0.2,
+}
+
+SIGNAL_VISUALIZATION_PARAMS = {
+    'channel_names': ["EEG (0.3-30Hz)", "Sigma (11-16Hz)", "Hilbert Envelope"],
+    'input_examples': 2
+}
